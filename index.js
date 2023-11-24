6810dc8203fd3a3bff0f4fe3f78a761865a8a91a
sohail <as19adj@herts.ac.uk>
2023-11-24 20:52:12 +0000

    modified index javaScrip

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
 //This is for mouse hover effect 
 let mouseFocus = document.querySelector(".focus");
+let documentContainer = document.querySelector(".container")
 
-    document.addEventListener("mousemove", function(mouseHover)
-    {
-        let x = mouseHover.pageX;
-        let y = mouseHover.pageY;
+if(  document.addEventListener("mousemove", function(mouseHover)
+{
+    let x = mouseHover.pageX;
+    let y = mouseHover.pageY;
 
-        mouseFocus.style.background = "radial-gradient(circle at "+x+"px "+y+'px ,rgba(94, 234, 212, 0.1), transparent 40%)'; 
+    mouseFocus.style.background = "radial-gradient(circle at "+x+"px "+y+'px ,rgba(94, 234, 212, 0.1), transparent 40%)'; 
 
-    })
+}));
+
+else{
+    mouseFocus.style.background = "rgba(94, 234, 212, 0.1)";
+}
+  
+
+
