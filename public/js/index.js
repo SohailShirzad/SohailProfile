9f20f25e3b4b6c4a1740a4f601511a1cd6ee838a
sohail <as19adj@herts.ac.uk>
2024-03-27 20:38:35 +0000

    adds projects to project section

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,6 @@
-
 //This is for mouse hover effect 
-let mouseFocus = document.querySelector(".focus");
-let documentContainer = document.querySelector(".container")
+const mouseFocus = document.querySelector(".focus");
+const documentContainer = document.querySelector(".container")
 
 if(  document.addEventListener("mousemove", function(mouseHover)
 {
@@ -16,7 +15,38 @@ else{
     mouseFocus.style.background = "rgba(94, 234, 212, 0.1)";
 }
 
-var height = document.querySelector("body");
-var focus = document.querySelector(".focus");
+const height = document.querySelector("body");
+const focus = document.querySelector(".focus");
 focus.style.height = height.offsetHeight + "px";
-  
+
+
+const projectSectionOne = document.getElementById("project-section-1");
+const projectSectionTwo = document.getElementById("project-section-2");
+const projectSectionThree = document.getElementById("project-section-3");
+const projectSectionFour = document.getElementById("project-section-4");
+
+projectSectionOne.setAttribute("target", "_blank");
+projectSectionTwo.setAttribute("target", "_blank");
+projectSectionThree.setAttribute("target", "_blank");
+projectSectionFour.setAttribute("target", "_blank");
+
+projectSectionOne.addEventListener("click" , function(){
+    const url = "https://github.com/SohailShirzad/";
+    window.open(url,"_blank","noopener");
+    console.log("hey");
+});
+
+projectSectionTwo.addEventListener("click", function(){
+    const url = "https://stroika.co.uk/index.html";
+    window.open(url,"_blank","noopener");
+});
+
+projectSectionThree.addEventListener("click", function(){
+    const url = "https://afghanassociationlondon.org.uk/";
+    window.open(url,"_blank","noopener");
+});
+
+projectSectionFour.addEventListener("click", function(){
+    const url = "https://sohailshirzad.github.io/Bootstrap-Portfolio/#Projects";
+    window.open(url,"_blank","noopener");
+});
